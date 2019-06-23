920f332034ca21e69c6b991f600c088ee594834e
Cristo Sparks <50341397+crisparks@users.noreply.github.com>
2019-06-23 08:45:50 -0500

    Add files via upload

---
+++ ListTester.java
@@ -0,0 +1,14 @@
+public class ListTester {
+    public static void main(String[] args) {
+        SinglyLinkedList a = new SinglyLinkedList();
+        a.add(1);
+        a.add(4);
+        a.add(3);
+        a.add(2);
+        a.add(11);
+        a.add(21);
+        a.remove();
+        a.remove();
+        a.printValues();
+    }
+}
